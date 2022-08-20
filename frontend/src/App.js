dd94cdb646ec4af3019ca8cd59daa5c81e169429
baolongt <tranbaolong14@gmail.com>
2022-08-20 14:15:44 +0700

    init FE

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { Button, notification } from "antd";
+const openNotification = () => {
+	notification.open({
+		message: "Notification Title",
+		description:
+			"This is the content of the notification. This is the content of the notification. This is the content of the notification.",
+		onClick: () => {
+			console.log("Notification Clicked!");
+		}
+	});
+};
+
+const App = () => {
+	return (
+		<>
+			<div>
+				<Button type="primary" onClick={openNotification}>
+					Open the notification box
+				</Button>
+			</div>
+			;
+		</>
+	);
+};
+
+export default App;
