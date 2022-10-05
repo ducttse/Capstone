3cf3596c60825476fea5bd7e24f3309d5ad32cfc
Long Tran <57053685+baolongt@users.noreply.github.com>
2022-10-05 19:11:17 +0700

    add questionForm state

---
+++ frontend/src/api/questions.js
@@ -0,0 +1,58 @@
+const questions = [
+	{
+		id: "1",
+		title: "Câu hỏi về cách mạng tháng ",
+		content: "Cách mạng tháng 8 xảy ra lúc nào, có ý nghĩa là gì?"
+	},
+	{
+		id: "2",
+		title: "Câu hỏi về cách mạng tháng ",
+		content: "Cách mạng tháng 8 xảy ra lúc nào, có ý nghĩa là gì?"
+	},
+	{
+		id: "3",
+		title: "Câu hỏi về cách mạng tháng ",
+		content: "Cách mạng tháng 8 xảy ra lúc nào, có ý nghĩa là gì?"
+	},
+	{
+		id: "4",
+		title: "Câu hỏi về cách mạng tháng ",
+		content: "Cách mạng tháng 8 xảy ra lúc nào, có ý nghĩa là gì?"
+	},
+	{
+		id: "5",
+		title: "Câu hỏi về cách mạng tháng ",
+		content: "Cách mạng tháng 8 xảy ra lúc nào, có ý nghĩa là gì?"
+	},
+	{
+		id: "6",
+		title: "Câu hỏi về cách mạng tháng ",
+		content: "Cách mạng tháng 8 xảy ra lúc nào, có ý nghĩa là gì?"
+	},
+	{
+		id: "7",
+		title: "Câu hỏi về cách mạng tháng ",
+		content: "Cách mạng tháng 8 xảy ra lúc nào, có ý nghĩa là gì?"
+	},
+	{
+		id: "8",
+		title: "Câu hỏi về cách mạng tháng ",
+		content: "Cách mạng tháng 8 xảy ra lúc nào, có ý nghĩa là gì?"
+	},
+	{
+		id: "9",
+		title: "Câu hỏi về cách mạng tháng ",
+		content: "Cách mạng tháng 8 xảy ra lúc nào, có ý nghĩa là gì?"
+	},
+	{
+		id: "10",
+		title: "Câu hỏi về cách mạng tháng ",
+		content: "Cách mạng tháng 8 xảy ra lúc nào, có ý nghĩa là gì?"
+	}
+];
+export const getQuestions = () => {
+	setTimeout(1000, () => {
+		console.log("call api");
+	});
+	return questions ? questions : [];
+};
---
+++ frontend/src/redux/actions/questionForm.action.js
@@ -0,0 +1,8 @@
+import { QUESTION_FORM_UPDATE } from "../constants/questionForm.constant.js";
+
+export function updateQuestionForm(payload) {
+	return {
+		type: QUESTION_FORM_UPDATE,
+		payload: payload
+	};
+}
