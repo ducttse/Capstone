acdaaf8a7701688bf3fd8eb6c7f0f5a40d6adead
Nguyen Ngoc Binh <binhnn1994@gmail.com>
2022-11-19 12:38:20 +0700

    fix authorize header

--- frontend/src/redux/auth/sagas/auth.saga.js
+++ frontend/src/redux/auth/sagas/auth.saga.js
@@ -1,11 +1,13 @@
 import { call, put } from "redux-saga/effects";
 import { checkLogin } from "../../../api/auth/authApi";
+import AxiosInstance from "../../../api/axiosInstance";
 import { loginFail, loginSuccess } from "../actions/auth.action";
 
 export function* loginAsync(action) {
 	try {
 		const user = yield call(checkLogin, action.payload);
 		yield put(loginSuccess(user));
+		AxiosInstance.defaults.headers.common['Authorization'] = `${user.tokenType} ${user.accessToken}`;
 	} catch (error) {
 		const message = "Email hoặc mật khẩu không đúng";
 		yield put(loginFail(message));
